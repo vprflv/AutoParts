059910b07c71645182fd044b47dc0849fa32d551
viktorprf234 <viktorprf234@gmail.com>
2026-05-13 23:26:49 +0700

    onPrisma stable 0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 import Footer from "@/src/common/components/Footer";
 import AuthProvider from "@/src/providers/AuthProvider";
+import {startMagicTokenCleanup} from "@/lib/cron/clean-magic-tokens";
 
 
 export const metadata: Metadata = {
@@ -15,6 +16,12 @@ export default function RootLayout({
                                    }: {
     children: React.ReactNode;
 }) {
+
+    if (typeof window === "undefined") {
+        startMagicTokenCleanup();
+    }
+
+
     return (
         <html lang="ru" className="dark">
         <body className="bg-background text-foreground antialiased min-h-screen">
